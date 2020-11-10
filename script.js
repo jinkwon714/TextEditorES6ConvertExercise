304ad3f774327010b51bd1872ecbfccac27f46c2
Hyukjin Kwon <hjnim0909@gmail.com>
2020-11-10 17:05:14 +0900

    Convert TextEditor Project to ES6+
    DONE: Convert all functions and any other possible elements to ES6

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//TODO: Convert all functions and any other possible elements to ES6
+//DONE: Convert all functions and any other possible elements to ES6
 
 /**
  * Update the output text as you type in the textarea
  */
-function updateText(){
+updateText = () => {
   let text = document.getElementById("text-input").value;
   document.getElementById('text-output').innerText = text;
 }
@@ -11,7 +11,7 @@ function updateText(){
 /**
  * Toggle the bold class for the output text
  */
-function makeBold(elem){
+makeBold = (elem) =>{
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('bold');
 }
@@ -19,7 +19,7 @@ function makeBold(elem){
 /**
  * Toggle the italic class for the output text
  */
-function makeItalic(elem){
+makeItalic = (elem) => {
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('italic');
 }
@@ -27,7 +27,7 @@ function makeItalic(elem){
 /**
  * Toggle the underline class for the output text
  */
-function makeUnderline(elem){
+makeUnderline = (elem) => {
     elem.classList.toggle('active');
     let output = document.getElementById('text-output');
     if(output.classList.contains('underline')){
@@ -41,11 +41,14 @@ function makeUnderline(elem){
  * Toggle the style textAlign attribute
  * Toggle the active state for the align butttons
  */
-function alignText(elem, alignType){
+alignText = (elem, alignType) => {
   document.getElementById('text-output').style.textAlign = alignType;
   let alignButtons = document.getElementsByClassName('align');
-  for(let i = 0; i < alignButtons.length; i++ ){
-    alignButtons[i].classList.remove('active');
+  for(let button of alignButtons) {           // ES6 Way
+    button.classList.remove('active')
   }
+  // for(let i = 0; i < alignButtons.length; i++ ){    // vanila JS
+  //   alignButtons[i].classList.remove('active');
+  // }
   elem.classList.toggle('active');
 }
